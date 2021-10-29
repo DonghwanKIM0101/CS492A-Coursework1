4e8c82d124932a1f2e34200d4c4c8074c87d74e3
DonghwanKIM0101 <65400489+DonghwanKIM0101@users.noreply.github.com>
2021-10-29 16:32:42 +0900

    Face reconstruction

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import time
 from train_test_split import split
 
-M = 416
+M = 100
 INDEX = 0 # image to reconstruct
 WIDTH = 46
 HEIGHT = 56
@@ -44,52 +44,85 @@ eig_val_low, eig_vec_low = solve_eig(S_low)
 
 print("elasped time is ", time.time() - start_time)
 
-# Check eigen vectors and eigen values are identical.
-eig_vec_error = 0
-eig_val_error = np.average(np.abs(eig_val[:M] - eig_val_low[:M]))
-for i in range(M):
-    u = eig_vec[:,i]
+# # Check eigen vectors and eigen values are identical.
+# eig_vec_error = 0
+# eig_val_error = np.average(np.abs(eig_val[:M] - eig_val_low[:M]))
+# for i in range(M):
+#     u = eig_vec[:,i]
 
-    u_low = eig_vec_low[:,i]
-    u_low = np.matmul(A, u_low)
-    u_low /= np.linalg.norm(u_low)
+#     u_low = eig_vec_low[:,i]
+#     u_low = np.matmul(A, u_low)
+#     u_low /= np.linalg.norm(u_low)
 
-    eig_vec_error += abs(1 - abs(np.dot(u, u_low) / (np.linalg.norm(u) * np.linalg.norm(u_low))))
+#     eig_vec_error += abs(1 - abs(np.dot(u, u_low) / (np.linalg.norm(u) * np.linalg.norm(u_low))))
 
-eig_vec_error /= M
-# Plot eigen values.
-print(eig_vec_error)
-print(eig_val_error)
-print(eig_vec.shape)
-print(eig_vec_low.shape)
-plt.plot(eig_val)
-plt.plot(eig_val_low)
-plt.show()
+# eig_vec_error /= M
+# # Plot eigen values.
+# print(eig_vec_error)
+# print(eig_val_error)
+# print(eig_vec.shape)
+# print(eig_vec_low.shape)
+# plt.plot(eig_val)
+# plt.plot(eig_val_low)
+# plt.show()
+
 
 # Face Reconstruction
-phi = A[:,INDEX]
+# phi = data_test[:,INDEX] - mean_flatten
+
+#     face_recon = mean_flatten
+
+#     for i in range(M):
+#         # u = eig_vec[:,i]
+
+#         u = eig_vec_low[:,i]
+#         u = np.matmul(A, u)
+#         u /= np.linalg.norm(u)
+
+#         a = np.dot(phi, u)
+#         face_recon += a * u
+
+#     # print("M is %d, reconstruction error is %f"%(M ,np.linalg.norm(face_recon - data_test[:,INDEX])))
+
+#     face_recon = face_recon.reshape((WIDTH,HEIGHT))
+#     face_recon = face_recon.transpose()
+
+#     cv2.imshow("mean face", mean_image)
+#     cv2.imshow("original", data_test[:,INDEX].reshape((WIDTH,HEIGHT)).transpose())
+
+#     cv2.imshow("face reconstruction", np.uint8(face_recon))
+#     cv2.waitKey(0)
+
+
+BASES = [50, 100]
+INDICES = [0, 2]
 
 face_recon = mean_flatten
+for index in INDICES:
+    phi = data_test[:,index] - mean_flatten
 
-for i in range(M):
-    u = eig_vec[:,i]
+    cv2.imwrite("../Figure/original_%d.jpg"%index, data_test[:,index].reshape((WIDTH,HEIGHT)).transpose())
 
-    # u = eig_vec_low[:,i]
-    # u = np.matmul(A, u)
-    # u /= np.linalg.norm(u)
+    for m in BASES:
+        face_recon_low = face_recon.copy()
+        face_recon_original = face_recon.copy()
+        print("PCA on bases, %d and index, %d ..."%(m, index))
 
-    a = np.dot(phi, u)
-    face_recon += a * u
+        for i in range(m):
+            u = eig_vec[:,i]
 
-    # cv2.imshow("test%d"%i, np.uint8((face_recon).reshape((WIDTH,HEIGHT)).transpose()))
+            u_low = eig_vec_low[:,i]
+            u_low = np.matmul(A, u_low)
+            u_low /= np.linalg.norm(u_low)
 
-print("M is %d, reconstruction error is %f"%(M ,np.linalg.norm(face_recon - data_train[:,INDEX])))
+            a = np.dot(phi, u)
+            a_low = np.dot(phi, u_low)
 
-face_recon = face_recon.reshape((WIDTH,HEIGHT))
-face_recon = face_recon.transpose()
+            face_recon_original +=  a * u
+            face_recon_low += a_low * u_low
 
-cv2.imshow("mean face", mean_image)
-cv2.imshow("original", data[:,:,INDEX])
+        face_recon_original = face_recon_original.reshape((WIDTH,HEIGHT)).transpose()
+        face_recon_low = face_recon_low.reshape((WIDTH,HEIGHT)).transpose()
 
-plt.imshow(face_recon, cmap='gist_gray')
-plt.show()
+        cv2.imwrite("../Figure/original_%d_%d.jpg"%(m, index), np.uint8(face_recon_original))
+        cv2.imwrite("../Figure/low_%d_%d.jpg"%(m, index), np.uint8(face_recon_low))
